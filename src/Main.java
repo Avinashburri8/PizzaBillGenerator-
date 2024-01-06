2765dd899b52869142a48a07fac593f537e991ee
Avinashburri8 <111901839+Avinashburri8@users.noreply.github.com>
2024-01-06 16:09:35 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main{
     public static void main(String[] args){
 
-        //Pizza basePizza = new Pizza(false);
-        //basePizza.addExtraCheese();
-        //basePizza.addExtraToppings();
-        //basePizza.takeaway();
-        //basePizza.getBill();
+        Pizza basePizza = new Pizza(false);
+        basePizza.addExtraCheese();
+        basePizza.addExtraToppings();
+        basePizza.takeaway();
+        basePizza.getBill();
 
         DeluxePizza dp = new DeluxePizza(false);
 
